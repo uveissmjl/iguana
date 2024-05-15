30fa74eb45440a03eeccedef09cbe229926acafc
uveissmjl <143725358+uveissmjl@users.noreply.github.com>
2024-05-15 15:03:09 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'iguana=iguana.py:main',
+            'iguana=iguana:main',
         ],
     },
     install_requires=[
